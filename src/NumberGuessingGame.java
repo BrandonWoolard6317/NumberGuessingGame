ca5073c291e699b23ff6a26a9b52f313f5b2f533
Brandon Woolard <brandon.woolard.2019@student.lhschools.org>
2018-10-05 10:02:25 -0400

    Ready for recording

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -175,15 +175,55 @@ public class NumberGuessingGame {
 			System.exit(0);
 		}*/
 
-		System.out.print("Do you wan to play my new game called The Number Guessing Game?\n1 Yes\n2 No");
+		//Loop for Number Guessing Game
+		System.out.print("Do you want to play my new game called The Number Guessing Game?\n1 Yes\n2 No");
 		usersInput = keyboard.nextLine();
 		yesOrNo = Integer.parseInt(usersInput);
-		if(yesOrNo == 1){
-			generatedNumber = (int) (Math.random() * 100);
-			System.out.println("Guess a number between 0-100.");
-			usersInput = keyboard.nextLine();
-			numberGuessing = Integer.parseInt(usersInput);
-			
+		if(yesOrNo == 0){
+			System.exit(0);
 		}
+		generatedNumber = (int) (Math.random() * 100);
+		System.out.println("Guess a number between 0-100.");
+		usersInput = keyboard.nextLine();
+		numberGuessing = Integer.parseInt(usersInput);
+		int a = 0;
+		boolean Loop = true;
+		while(Loop){
+			a++;
+			if(numberGuessing == generatedNumber){
+				if(a == 1){
+					System.out.println("Congrats! You won using only 1 attempt!");
+				} else{
+					System.out.println("Congrats! You won using "+a+" attempts!");
+				}
+				System.out.println("\nDo you want to play again?\n1 For Yes\n2 For No");
+				usersInput = keyboard.nextLine();
+				yesOrNo = Integer.parseInt(usersInput);
+				if(yesOrNo == 2) {
+					System.exit(0);
+				} else if(yesOrNo == 1){
+					generatedNumber = (int) (Math.random() * 100);
+					System.out.println("Guess a new number between 0-100.");
+					usersInput = keyboard.nextLine();
+					numberGuessing = Integer.parseInt(usersInput);
+					a = 1;
+				}
+			}
+
+			if(a > 1){
+				System.out.println("You're on attempt "+a+".");
+			}
+
+			if(numberGuessing > generatedNumber){
+				System.out.println("Your guess was too high. Try again.");
+				usersInput = keyboard.nextLine();
+				numberGuessing = Integer.parseInt(usersInput);
+			} else if(numberGuessing < generatedNumber){
+				System.out.println("Your guess was too low. Try again.");
+				usersInput = keyboard.nextLine();
+				numberGuessing = Integer.parseInt(usersInput);
+			}
+		}
+		//End of Loop
 	}
 }
